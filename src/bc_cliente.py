6f97ac2d69135ec6c8aadc39d019161a0dbd1bcd
Diego Ismael <disma.77@hotmail.com>
2016-03-21 11:09:15 -0600

    Agregada recepcion de mensajes
    Se agrego el hilo encargado de la recepcion de mensajes y
    se implemento la base de lo que sera el siste de recepcion y
    procesado de mensjaes. Para muestra se tiene el mensaje de temperatura
    del servidor remoto, el cual se actualiza cada segundo y se muestra en
    un control de texto dentro de la GUI principal.
    
    TODO: Falta buscar la manera de desconectarse del servidor, al parecer
    no funciona la desconeccion.

--- src/bc_cliente.py
+++ src/bc_cliente.py
@@ -32,6 +32,25 @@ class Sender(QtCore.QObject):
         logging.debug('Finalizando envio')
         self.finished.emit()
 
+class Receiver(QtCore.QObject):
+    finished = QtCore.pyqtSignal()
+    messagein = QtCore.pyqtSignal(int, int, QtCore.QString)
+    def __init__(self, conex):
+        super(Receiver, self).__init__()
+        self.conex = conex
+
+    def receiver(self):
+        logging.debug('Comenzando recepcion de datos')
+        msg = ''
+        while True:
+            mm, msg = ms.getMessageFromClient(self.conex, msg)
+            if mm.getTipo() == 0 and mm.getSubtipo() == 0:
+                break
+            else:
+                self.messagein.emit(mm.getTipo(), mm.getSubtipo(), QtCore.QString(mm.getDatos()))
+        logging.debug('Finalizando recepcion')
+        self.finished.emit()
+
 
 class FrmCliente(QtGui.QMainWindow):
     def __init__(self):
@@ -40,25 +59,64 @@ class FrmCliente(QtGui.QMainWindow):
         self.btnConectar.clicked.connect(self.btnConectar_click)
         
     def btnConectar_click(self):
+        if self.btnConectar.text() == 'Conectar':
+            c = self.conectar()
+            if c:
+                self.btnConectar.setText('Desconectar')
+        else:
+            c = self.desconectar()
+            if c:
+                self.btnConectar.setText('Conectar')
+    def desconectar(self):
+        self.tmrTemp.stop()
+        self.tSndr.quit()
+        self.tRcvr.quit()
+        self.conex.close()
+        return True
+
+    def conectar(self):
         srv = self.txtIP.text()
         conex = socket.socket()
         try:
             conex.connect((srv, 2014))
         except socket.error:
             print ('No se pudo conectar al servidor')
-            return
-
+            return False
+        self.conex = conex #guardamos la conexion
         self.colaEnvio = Queue()
         # Creando hilos
         logging.debug('Creando hilos')
+        
         self.tSndr = QtCore.QThread()
         self.sndr = Sender(conex, self.colaEnvio)
         self.sndr.moveToThread(self.tSndr)
         self.tSndr.started.connect(self.sndr.sender)
         self.sndr.finished.connect(self.tSndr.quit)
         self.tSndr.start()
-        self.colaEnvio.put(ms.Message(1,2,'Hola mundo del servidor'))
 
+        self.tRcvr = QtCore.QThread()
+        self.rcvr = Receiver(conex)
+        self.rcvr.moveToThread(self.tRcvr)
+        self.tRcvr.started.connect(self.rcvr.receiver)
+        self.rcvr.finished.connect(self.tRcvr.quit)
+        self.rcvr.messagein.connect(self.procesarMensaje)
+        self.tRcvr.start()
+
+        self.tmrTemp = QtCore.QTimer()
+        self.tmrTemp.timeout.connect(self.pedirTemperatura)
+        self.tmrTemp.start(1000)
+        return True
+
+
+
+    def pedirTemperatura(self):
+        self.colaEnvio.put(ms.Message(11,1))
+
+    def procesarMensaje(self, tipo, subtipo, datos):
+        if tipo == 1:
+            if subtipo == 1: #La temperatura
+                self.txtTemp.setText(datos)
+                
 
 if __name__ == '__main__':
     logging.basicConfig(level = logging.DEBUG, 
